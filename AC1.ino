0458fc33dc002e1f20099e6e63ef3c5c77396c12
Celks <78734896+Celks@users.noreply.github.com>
2021-03-28 12:39:10 -0300

    AC1.ino

--- AC1.ino
+++ AC1.ino
@@ -2,10 +2,10 @@
 const int vermelho = 5;
 const int verde = 6;
 const int azul = 7;
-
+//variaveis dos botoes
 bool estadoLedVermelho = false;
-
 const int botao1 = 2;
+const int botao2 = 3;
 unsigned long lastDebounceTime1 = 0;
 const int botaoDelay = 100;
 
@@ -18,11 +18,11 @@ void setup()
   pinMode(verde, OUTPUT);
   pinMode(azul, OUTPUT);
   
-  Serial.begin(9600);
-	
+  Serial.begin(9600);//disponibilizar o uso do serial
+	//escrever as informações entre parenteses no serial
   Serial.println("AC1 - Meu Primeiro Projeto 2021");
   Serial.println("V1.0");
-  Serial.println("Grupo: NOME AQUI");
+  Serial.println("FECHADOES COM CRISTO");
 }
 
 void loop()
@@ -31,32 +31,63 @@ void loop()
   	Serial.println("botao 1 apertado");
     ledVermelho();
   	lastDebounceTime1 = millis();
+  //Caso o botao1 for pressionado, o Led vermelho se acendera
+  }
+ 
+  if((millis() - lastDebounceTime1) > botaoDelay && digitalRead(botao2)){
+  	Serial.println("botao 2 apertado");
+    ledVermelho();
+  	lastDebounceTime1 = millis();
+  //Caso o botao2 for pressionado o led vermelho se apagara
+// O contrario tambem se aplica.
+// Caso o botao 2 for pressionado o led acendera
+   //Caso o botao 1 for pressionado o led vermelho apagara
+  }
+ 
+  if(getTemperatura() > 15){
+    ledAzul(true);
+   Serial.println("A temperatura ultrapassou 15 graus");
+  delay(2000);
+  }else{
+  	ledAzul(false); 
+  //Caso a temperatura ultrapasse 15 graus, o led azul se acendera e será informado no serial que a temperatura limite foi atingida
+
   }
   
-  if(getTemperatura() > 30){
-    ledAzul(true);
+  if(getLuminosidade() > 5){
+    ledVerde(true);
+   Serial.println("A luminosidade ultrapassou de 5"); 
+    delay(2000);
   }else{
-  	ledAzul(falso); 
+  	ledVerde(false); 
+  //caso a luminosidade ultrapasse 5, o led verde se acendera e será informado no serial que a luminosidade limite foi atingida
+
   }
-  	
-  delay(10);
-}
+    
+  
+ 
+    
+  }
 
 void ledVermelho(){
   estadoLedVermelho = !estadoLedVermelho;
   digitalWrite(vermelho,estadoLedVermelho);
+//Função do led vermelho
 }
-void ledVerde(){
- //precisa fazer  
+void ledVerde(bool estado){
+ digitalWrite(verde, estado); 
+//função do led verde
 }
 void ledAzul(bool estado){
 	digitalWrite(azul,estado);
+  //função do led azul
 }
 
 int getTemperatura(){
   	int temperaturaC;
 	temperaturaC = map(((analogRead(A0) - 20) * 3.04), 0, 1023, -40, 125);
   	return temperaturaC;
+  //função de leitura da temperatura
 } 
 //funcao de leitura da luminosidade
 int getLuminosidade(){
